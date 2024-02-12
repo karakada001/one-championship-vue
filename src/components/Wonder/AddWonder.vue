9df7cf1a66f04c64ceb48a360b8ab890b25ac6b7
karakada001 <gotokron001@gmail.com>
2024-02-12 16:36:53 +0700

    first commit

---
+++ src/components/Wonder/AddWonder.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="container mt-4">
+        <div class="row">
+
+            <div class="col-md-5 mb-1 mx-auto">
+                <div class="card shadow">
+                    <div class="card-header p-0">
+                        <img src="https://cdn.onefc.com/wp-content/uploads/sites/5/2022/07/live-in-hd-one-championship-x-prime-video-official-press-conference.jpg" class="card-img-top" alt="wonders">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 mx-auto">
+                <div class="card shadow">
+                    <div class="card-header bg-secondary-subtle">
+                        <span class="text-white fs-5">
+                            ลงทะเบียน one Championship
+                        </span>
+                    </div>
+                    <div class="card-body text-success">
+                        <form @submit.prevent="addWonder">
+                            <div class="mb-1">
+                                <label for="">ชื่อนักกีฬา</label>
+                                <input type="text" class="form-control" v-model="wonderState.name" />
+                            </div>
+                            <div class="mb-1">
+                            </div>
+                            <div class="mb-1">
+                                <label for="">ชื่อค่าย</label>
+                                <input type="text" class="form-control" v-model="wonderState.imageUrl" />
+                            </div>
+
+                            <div class="mb-1">
+
+                                <button type="submit" class="btn btn-primary text-white shadow">
+                                    ลงทะเบียน
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+
+const wonderState = reactive({
+    name: '',
+    location: '',
+    imageUrl:''
+})
+
+const router = useRouter()
+
+const addWonder = async () => {
+    await axios.post(`https://json-server-vue3-d109.onrender.com/wonders`,wonderState)
+        .then(() => {
+            console.log(wonderState)
+            router.push({name:'wonders'})
+        })
+    alert('ลงทะเบียน เรียบร้อยแล้ว')
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Sarabun&display=swap');
+
+.container {
+    font-family: 'Sarabun', sans-serif;
+
+}
+
+.form-label {
+    color: rgb(247, 42, 6);
+}
+</style>
---
+++ src/views/layouts/NavBar.vue
@@ -0,0 +1,110 @@
+<script setup>
+import router from '@/router';
+import { RouterLink } from 'vue-router'
+
+const logOut = async () => {
+    localStorage.clear()
+    await router.push({ name: 'signin' })
+}
+
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark shadow" style="background: #7f00ff;">
+        <div class="container">
+            <RouterLink :to="{ name: 'home' }" class="navbar-brand">ONE Championship</RouterLink>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <RouterLink :to="{ name: 'home' }" class="nav-link active" aria-current="page"><i
+                                class="bi bi-house-fill me-1"></i>หน้าแรก</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'signin' }" class="nav-link" href="#"><i
+                                class="bi bi-bank me-1"></i>เข้าสู่ระบบ</router-link>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <i class="bi bi-bank2 me-1"></i>เกี่ยวกับ ONE
+                        </a>
+
+                        <ul class="dropdown-menu">
+                            <li>
+                                <RouterLink :to="{ name: 'wonders' }" class="dropdown-item">เกี่ยวกับ ONE 
+                                </RouterLink>
+                            </li>
+                            <li>
+                                <RouterLink :to="{ name: 'add-wonder' }" class="dropdown-item">รายชื่อ นักกีฬา
+                                </RouterLink>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="https://www.onefc.com/th/">เว็บไซต์ ONE Championship</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <i class="bi bi-bank2 me-1"></i>ซื้อบัตร
+                        </a>
+                        <ul class="dropdown-menu">
+
+                            <li>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">ติดต่อเรา</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            ตารางการแข่งขัน
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item"
+                                    href="https://www.onefc.com/th/events/"
+                                    target="_blank">อีเว้นทั้งหมด</a></li>
+                            <li><a class="dropdown-item"
+                                    href="https://www.onefc.com/th/events/#upcoming"
+                                    target="_blank">อีเว้นถัดไป</a></li>
+                            <li><a class="dropdown-item"
+                                    href="https://www.onefc.com/th/events/#past"
+                                    target="_blank">อีเว้นที่ผ่านมา</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item"
+                            ></a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">เว็บอื่นๆ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" @click="logOut"><i
+                                class="bi bi-box-arrow-right me-1"></i>ออกจากระบบ</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Sarabun&display=swap');
+
+.navbar {
+    font-family: 'Sarabun', sans-serif;
+}
+</style>
