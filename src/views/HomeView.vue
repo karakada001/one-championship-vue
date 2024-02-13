b06c8caa4f493ddc4a86c9e5c245261491b72137
karakada001 <gotokron001@gmail.com>
2024-02-13 10:31:12 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,7 +18,9 @@ toast.info("we", {
       <img src="https://cdn.onefc.com/wp-content/uploads/sites/5/2024/02/240216-BKK-OFF52-1800x1200px-1536x1024.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">One Championship</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text">(FRI) 16 FEB 19:30 น. ตามเวลาไทย
+สนามมวยเวทีลุมพินี, กรุงเทพฯ
+ONE ลุมพินี 52</p>
       </div>
       <div class="card-footer">
         <small class="text-body-secondary">Last updated 3 mins ago</small>
@@ -30,7 +32,9 @@ toast.info("we", {
       <img src="https://cdn.onefc.com/wp-content/uploads/sites/5/2024/01/240217-OFN19-1800x1200px-1536x1024.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">One Championship</h5>
-        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text">(SAT) 17 FEB 08:00 น. ตามเวลาไทย
+สนามมวยเวทีลุมพินี, กรุงเทพฯ
+ONE Fight Night 19: โจนาธาน vs ฟิลิปเป</p>
       </div>
       <div class="card-footer">
         <small class="text-body-secondary">Last updated 3 mins ago</small>
@@ -42,7 +46,9 @@ toast.info("we", {
       <img src="https://cdn.onefc.com/wp-content/uploads/sites/5/2023/12/240301-DOH-ONE166-1800x1200px-1-1536x1024.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">One Championship</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <p class="card-text">(FRI) 1 MAR 19:30 น. ตามเวลาไทย
+ลูเซล สปอร์ตส์ อารีนา, ลูเซล
+ONE 166: Qatar</p>
       </div>
       <div class="card-footer">
         <small class="text-body-secondary">Last updated 3 mins ago</small>
