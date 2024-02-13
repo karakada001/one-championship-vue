f9f393ae7108f5b4f14dc77117d22abf4d22c7af
karakada001 <gotokron001@gmail.com>
2024-02-13 14:16:14 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,10 +15,10 @@ toast.info("we", {
 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAxFBWM6P2zUd7WFdFaAunGiiNKmJwcjmf9g&usqp=CAU" class="d-block w-100" alt="...">
+      <img src="https://cdn.onefc.com/wp-content/uploads/sites/5/2023/11/231222-BKK-OFF46-1800x1200px.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQw2KJsb1SL0QVJr2Fr4Yx8nZBHGI2D0I-4g&usqp=CAU" class="d-block w-100" alt="...">
+      <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*OVqFtPZrAAknrve32h26Lw.jpeg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
       <img src="https://cdn.onefc.com/wp-content/uploads/sites/5/2021/06/220326-web-1800x1200px.jpeg" class="d-block w-100" alt="...">
