608e7fb67ab8955d8d13d002976b56cc859eb570
karakada001 <gotokron001@gmail.com>
2024-02-13 09:29:29 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,7 +15,7 @@ toast.info("we", {
   <div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
     <div class="card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="https://cdn.onefc.com/wp-content/uploads/sites/5/2024/02/240216-BKK-OFF52-1800x1200px-1536x1024.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">One Championship</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -27,7 +27,7 @@ toast.info("we", {
   </div>
   <div class="col">
     <div class="card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="https://cdn.onefc.com/wp-content/uploads/sites/5/2024/01/240217-OFN19-1800x1200px-1536x1024.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">One Championship</h5>
         <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -39,7 +39,7 @@ toast.info("we", {
   </div>
   <div class="col">
     <div class="card h-100">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="https://cdn.onefc.com/wp-content/uploads/sites/5/2023/12/240301-DOH-ONE166-1800x1200px-1-1536x1024.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">One Championship</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
